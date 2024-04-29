4450b8d8e0e741b210e66f71276a6fbc1fec5ff6
Subh Chaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-04-29 12:22:59 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,31 @@
-function onClickGenerateSentiment() {
+console.log("Activated");
+
+const onClickGenerateSentiment=async ()=>{
     console.log("sentiment button clicked");
     var handle = document.getElementById("handlevalue");
     console.log(handle);
-    var url = "http://127.0.0.1:5000/get_sentimentanalysis_overall"; //Use this if NOT using nginx
+    console.log(handle.value);
+    var url = "http://127.0.0.1:4996/get_sentimentanalysis_overall"; //Use this if NOT using nginx
     // var url = "/api/get_sentimentanalysis_overall"; // Use this if using nginx
 
-    var estPrice = document.getElementById("resultshowcase1");
+    var resulted = document.getElementById("resultshowcase1");
 
-    $.post(url, {
-        username: handle.value
-    },function(data, status) {
-        console.log(data);
-        estPrice.innerHTML = "<h2 class=\"result1\"> The Average Sentiment is: "+data.avg_sentiment_text.toString()+" with a specific value of "+data.avg_sentiment_float.toString()+"</h2>";
-        console.log(status);    
-    });
-    console.log("Reached End -1");
+    
+    resulted.innerHTML ="<h2 class=\"result1\">Loading.....</h2>"
+    const data = new FormData
+    data.append("username",handle.value)
+
+    const res = await fetch(url,{
+        method: 'POST',
+        body:data
+    })
+    const res_data = await res.json()
+    console.log(res_data );
+    resulted.innerHTML = "<h2 class=\"result1\"> The Average Sentiment is: "+res_data.avg_sentiment_text.toString()+" with a specific value of "+res_data.avg_sentiment_float.toString()+"</h2>";
+
+
+    console.log("kslfksjbb")
 
 }
 
-// window.onload = onPageLoad;
+// window.onload = onPageLoad
